cbe2f600d1ea713f30262ff9a6c7292826467b66
lorenzo.brutti <lorenzobrutti80@gmail.com>
2021-02-13 15:31:20 +0100

    test riduzione attributi da json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
     production: false,
     mapbox: {
         accessToken: 'pk.eyJ1IjoibGJydXR0aSIsImEiOiJjaXl5NXMxNWswMDA2MzJwZGViZDNxOG05In0.R5OZB0r3ov9HeduzarkLjw',
-        style: 'mapbox://styles/lbrutti/ckioi1m7m4o0g17nrz5wydq5g/draft'
+        style: 'mapbox://styles/lbrutti/ckioi1m7m4o0g17nrz5wydq5g/draft?optimize=true'
     },
     server:{
         protocol:"http",
