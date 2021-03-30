cc463900f866e96e1c23680f705b1580d2af04bc
lorenzo.brutti <lorenzobrutti80@gmail.com>
2021-03-30 14:07:38 +0200

    commento

--- src/app/services/monithonMapService/monithonmap.service.ts
+++ src/app/services/monithonMapService/monithonmap.service.ts
@@ -71,6 +71,7 @@ export class MonithonMapService {
             language: 'it',
             //zoom: 3,
             filter: function (item: any) {
+                //workaround per non escludere aosta
                 return (item.place_type[0] === 'place' || item.place_type[0] === 'region');
             },
             marker: false
