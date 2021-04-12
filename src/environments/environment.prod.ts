1b1b5db467380092ac4d4cad644aadb86089037b
lorenzo.brutti <lorenzobrutti80@gmail.com>
2021-04-12 13:15:17 +0200

    inserito variabili intestate a monithon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: true,
     monithonReportUrl: 'https://it.monithon.eu/report/view',
     mapbox: {
-        accessToken: 'pk.eyJ1IjoibGJydXR0aSIsImEiOiJjaXl5NXMxNWswMDA2MzJwZGViZDNxOG05In0.R5OZB0r3ov9HeduzarkLjw',
-        style: 'mapbox://styles/lbrutti/ckioi1m7m4o0g17nrz5wydq5g/draft?optimize=true'
+        accessToken: 'pk.eyJ1IjoibW9uaXRob24iLCJhIjoiY2tuZWNsb2Y0MHdvejJwbzdzYWtoam9ociJ9.UNMDQ62eNCvZPoC0ITNOzA',
+        style: 'mapbox://styles/monithon/cknee3k8n310r17s7ewa1coel?optimize=true'
     },
     server: {
         protocol: "https",
