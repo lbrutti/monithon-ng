1fd1341c762d039a85a37543eef59d4cce72679a
lorenzo.brutti <lorenzobrutti80@gmail.com>
2021-04-24 19:46:59 +0200

    modificato rilevamento mobile con libreria MobileDetect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { TranslocoService } from '@ngneat/transloco';
 import { Router } from '@angular/router';
 
+import * as MobileDetect from 'mobile-detect';
+import lodash from 'lodash';
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html',
@@ -24,13 +25,33 @@ export class AppComponent {
 
     initializeApp() {
         this.platform.ready().then(() => {
+
+            let hasTouchScreen = false;
+            if ("maxTouchPoints" in navigator) {
+                hasTouchScreen = navigator.maxTouchPoints > 0;
+            } else if ("msMaxTouchPoints" in navigator) {
+                hasTouchScreen = navigator['msMaxTouchPoints'] > 0;
+            } else {
+                let mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+                if (mQ && mQ.media === "(pointer:coarse)") {
+                    hasTouchScreen = !!mQ.matches;
+                } else if ('orientation' in window) {
+                    hasTouchScreen = true; // deprecated, but good fallback
+                }
+            }
+
+            const md = new MobileDetect(window.navigator.userAgent);
+            const isMobileDetected = !lodash.isNil(md.mobile()) || !lodash.isNil(md.phone());
+            // let isTablet = !lodash.isNil(md.tablet());
+
+            if (isMobileDetected) {
+                this.router.navigate(['/courtesy'], { skipLocationChange: true });
+            }
+
             this.translocoService.setDefaultLang('it');
             this.translocoService.setActiveLang('it');
             this.statusBar.styleDefault();
 
-            if (!this.platform.is('desktop')) {
-                this.router.navigate(['/courtesy']);
-            }
 
         });
     }
