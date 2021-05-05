92ec1a877a7e538d7f1061db981f7ec931660c45
lorenzo.brutti <lorenzobrutti80@gmail.com>
2021-05-05 14:35:05 +0200

    aggiunto mock temi sintetici

---
+++ src/assets/mock/temiSintetici.js
@@ -0,0 +1,56 @@
+let cicloProgrammazione = [
+    {
+        "codTemaSintetico": 1,
+        "descTemaSintetico": "Trasporti e infrastrutture a rete"
+    },
+    {
+        "codTemaSintetico": 2,
+        "descTemaSintetico": "Ambiente e prevenzione dei rischi"
+    },
+    {
+        "codTemaSintetico": 3,
+        "descTemaSintetico": "Attrazione culturale, naturale e turistica"
+    },
+    {
+        "codTemaSintetico": 4,
+        "descTemaSintetico": "Competitività per le imprese"
+    },
+    {
+        "codTemaSintetico": 5,
+        "descTemaSintetico": "Ricerca e innovazione"
+    },
+    {
+        "codTemaSintetico": 6,
+        "descTemaSintetico": "Energia e efficienza energetica"
+    },
+    {
+        "codTemaSintetico": 7,
+        "descTemaSintetico": "Rafforzamento capacità della PA"
+    },
+    {
+        "codTemaSintetico": 8,
+        "descTemaSintetico": "Istruzione"
+    },
+    {
+        "codTemaSintetico": 9,
+        "descTemaSintetico": "Occupazione e mobilità dei lavoratori"
+    },
+    {
+        "codTemaSintetico": 10,
+        "descTemaSintetico": "Inclusione sociale"
+    },
+    {
+        "codTemaSintetico": 11,
+        "descTemaSintetico": "Agenda digitale"
+    },
+    {
+        "codTemaSintetico": 12,
+        "descTemaSintetico": "Rinnovamento urbano e rurale"
+    },
+    {
+        "codTemaSintetico": 13,
+        "descTemaSintetico": "Servizi di cura infanzia e anziani"
+    }
+];
+
+export default cicloProgrammazione;
