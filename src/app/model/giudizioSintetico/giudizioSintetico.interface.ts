3e4e75332d99d82769e11a5b971a1b77e4dd832f
lorenzo.brutti <lorenzobrutti80@gmail.com>
2021-05-05 15:54:03 +0200

    creato modello per tema sintentico

---
+++ src/app/model/giudizioSintetico/giudizioSintetico.interface.ts
@@ -0,0 +1,8 @@
+
+// da ricevere da ws
+export interface GiudizioSintetico {
+    codGiudizioSintetico: number;
+    descGiudizioSintetico: string;
+    isSelected: boolean;
+    isActive:boolean;
+}
---
+++ src/app/model/temaSintetico/temaSintetico.ts
@@ -0,0 +1,6 @@
+export interface TemaSintetico {
+    codTemaSintetico: string | number;
+    descTemaSintetico: string;
+    isActive: boolean;
+    isSelected: boolean;
+}
