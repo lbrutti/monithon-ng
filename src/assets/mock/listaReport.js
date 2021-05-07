e5d3f24ca96800230308e9e954bf440a6611f392
lorenzo.brutti <lorenzobrutti80@gmail.com>
2021-05-07 15:40:07 +0200

    aggiunto stub response ws report

---
+++ src/assets/mock/listaReport.js
@@ -0,0 +1,15 @@
+[
+    
+    {
+        "uid": 1144,
+        "titoloReport": "Palazzo delle Aquile",
+        "dataInserimento": "27/04/2021",
+        "codGiudizioSintetico": 1,
+        "ocCodTemaSintetico": "06",
+        "ocFinanzTotPubNetto": 8150000,
+        "ocCodProgrammaOperativo": "2007SI002FAPA1",
+        "ocCodCicloProgrammazione": 1,
+        "lat": 38.1154024,
+        "long": 13.362076093824822
+    }
+]
