3f7b0f063cce951ecad73e0a420e020d8a887c2e
lorenzo.brutti <lorenzobrutti80@gmail.com>
2021-05-28 16:33:15 +0200

    creato mock ws dettaglio report

--- src/assets/mock/dettaglioReport.js
+++ src/assets/mock/dettaglioReport.js
@@ -3,13 +3,14 @@
     {
         "uid": 1144,
         "titoloReport": "Palazzo delle Aquile",
-        "sintesiReport": "SI TRATTA DI MANUTENZIONE STRAORDINARIA ED ADEGUAMENTO SICUREZZA LUOGHI DI LAVORO, COMPRESI IMPIANTI, ma attraverso tali lavori si vuole incentivare e rilanciare uno dei luoghi simbolo della città di Palermo",
         "dataInserimento": "27/04/2021",
         "codGiudizioSintetico": 1,
-        "link": "https://it.monithon.eu/report/view/1144",
         "ocCodTemaSintetico": "06",
         "ocFinanzTotPubNetto": 8150000,
         "ocCodProgrammaOperativo": "2007SI002FAPA1",
-        "ocCodCicloProgrammazione": 1
+        "ocCodCicloProgrammazione": 1,
+
+        "sintesiReport": "SI TRATTA DI MANUTENZIONE STRAORDINARIA ED ADEGUAMENTO SICUREZZA LUOGHI DI LAVORO, COMPRESI IMPIANTI, ma attraverso tali lavori si vuole incentivare e rilanciare uno dei luoghi simbolo della città di Palermo",
+        "link": "https://it.monithon.eu/report/view/1144"
     }
 ]
