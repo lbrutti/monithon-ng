4a8fc41dc33bc86d9c969dcc09a1d68047017cc9
lorenzo.brutti <lorenzobrutti80@gmail.com>
2021-05-31 18:26:07 +0200

    aggiunto script di build dev e sostituito temporaneamente apikey in env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,13 @@ export const environment = {
     production: true,
     monithonReportUrl: 'https://it.monithon.eu/report/view',
     mapbox: {
-        accessToken: 'pk.eyJ1IjoibW9uaXRob24iLCJhIjoiY2tuZWNsb2Y0MHdvejJwbzdzYWtoam9ociJ9.UNMDQ62eNCvZPoC0ITNOzA',
-        style: 'mapbox://styles/monithon/cknee3k8n310r17s7ewa1coel?optimize=true',
+        // accessToken: 'pk.eyJ1IjoibW9uaXRob24iLCJhIjoiY2tuZWNsb2Y0MHdvejJwbzdzYWtoam9ociJ9.UNMDQ62eNCvZPoC0ITNOzA',
+        // style: 'mapbox://styles/monithon/cknee3k8n310r17s7ewa1coel?optimize=true',
+        // reportStyle: 'mapbox://styles/lbrutti/ckpcrsy37511318o48aiagzcz?optimize=true'
+
+        
+        accessToken: 'pk.eyJ1IjoibGJydXR0aSIsImEiOiJjaXl5NXMxNWswMDA2MzJwZGViZDNxOG05In0.R5OZB0r3ov9HeduzarkLjw',
+        style: 'mapbox://styles/lbrutti/ckioi1m7m4o0g17nrz5wydq5g/draft?optimize=true',
         reportStyle: 'mapbox://styles/lbrutti/ckpcrsy37511318o48aiagzcz?optimize=true'
 
     },
