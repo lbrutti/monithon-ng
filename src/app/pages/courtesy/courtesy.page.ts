c0ef73d3c925013c08c7988a88341572cf988cb8
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2021-06-25 17:25:37 +0200

    parametrizzato sottotitolo in courtesy

--- src/app/pages/courtesy/courtesy.page.ts
+++ src/app/pages/courtesy/courtesy.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { destination } from '@turf/turf';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -11,11 +12,14 @@ export class CourtesyPage implements OnInit {
 
     public version: string = environment.version;
     public destination: string = '/';
-
+    public mode = 'projectFinder';
     constructor(private route: ActivatedRoute) { }
 
     ngOnInit() {
-        this.route.params.subscribe((params: Params) => this.destination = params['destination']);
+        this.route.params.subscribe((params: Params) => {
+            this.destination = params['destination'];
+            this.mode = this.destination.match(/report-finder/) ? 'reportFinder' : 'projectFinder';
+        });
     }
 
 }
