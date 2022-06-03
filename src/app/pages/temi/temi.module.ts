c49ead829432b4777202619db342b37fecfe2486
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2022-06-03 16:56:08 +0200

    SM-102: implmentata navigazione e colore bottoni

--- src/app/pages/temi/temi.module.ts
+++ src/app/pages/temi/temi.module.ts
@@ -7,14 +7,18 @@ import { IonicModule } from '@ionic/angular';
 import { TemiPageRoutingModule } from './temi-routing.module';
 
 import { TemiPage } from './temi.page';
+import { MatChipsModule } from '@angular/material/chips';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    TemiPageRoutingModule
-  ],
-  declarations: [TemiPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        TemiPageRoutingModule,
+        MatChipsModule,
+        TranslocoModule
+    ],
+    declarations: [TemiPage]
 })
-export class TemiPageModule {}
+export class TemiPageModule { }
--- src/app/pages/temi/temi.page.ts
+++ src/app/pages/temi/temi.page.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Tema } from 'src/app/model/tema/tema.interface';
+import { MonithonApiService } from 'src/app/services/monithonApiService/monithon-api.service';
 
 @Component({
-  selector: 'app-temi',
-  templateUrl: './temi.page.html',
-  styleUrls: ['./temi.page.scss'],
+    selector: 'app-temi',
+    templateUrl: './temi.page.html',
+    styleUrls: ['./temi.page.scss'],
 })
-export class TemiPage implements OnInit {
+export class TemiPage implements OnInit, AfterViewInit {
+    temi: Array<Tema>;
 
-  constructor() { }
+    constructor(private monithonApiService: MonithonApiService, private router:Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+    ngAfterViewInit() {
+        this.monithonApiService.getTemi().toPromise()
+            .then(data => {
+                this.temi = data.temi;
+                //create css variables for temi:
+                this.temi.map((t:any) => {
+                    document.documentElement.style.setProperty(`--monithon-tema-${t.ocCodTemaSintetico}-background`, t.stile.colore);
+                });
+            });
+
+    }
+
+    public goToTemaPreset(tema: Tema) {
+        this.router.navigateByUrl(`/tema/${tema.ocCodTemaSintetico}`);
+    }
 
 }
