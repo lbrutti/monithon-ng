ccb15220d60b1ff3b4e4c06700d3b58b9c2042e8
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2022-06-14 22:34:50 +0200

    SM-96: aggiunto valutazione piattaforma

--- src/app/pages/temi/temi.page.ts
+++ src/app/pages/temi/temi.page.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
 import { Tema } from 'src/app/model/tema/tema.interface';
 import { MonithonApiService } from 'src/app/services/monithonApiService/monithon-api.service';
 
@@ -10,17 +11,26 @@ import { MonithonApiService } from 'src/app/services/monithonApiService/monithon
 })
 export class TemiPage implements OnInit, AfterViewInit {
     temi: Array<Tema>;
+    isMobile: boolean = false;
 
-    constructor(private monithonApiService: MonithonApiService, private router:Router) { }
+    constructor(private monithonApiService: MonithonApiService, private router: Router, private platform: Platform) { }
 
     ngOnInit() {
+        let hasTouchScreen = false;
+        if ("maxTouchPoints" in navigator) {
+            hasTouchScreen = navigator.maxTouchPoints > 0;
+        } else if ("msMaxTouchPoints" in navigator) {
+            hasTouchScreen = navigator['msMaxTouchPoints'] > 0;
+        }
+        let goodDevice = this.platform.is('desktop') || this.platform.is('tablet') || !hasTouchScreen;
+        this.isMobile = !goodDevice;
     }
     ngAfterViewInit() {
         this.monithonApiService.getTemi().toPromise()
             .then(data => {
                 this.temi = data.temi;
                 //create css variables for temi:
-                this.temi.map((t:any) => {
+                this.temi.map((t: any) => {
                     document.documentElement.style.setProperty(`--monithon-tema-${t.ocCodTemaSintetico}-background`, t.stile.colore);
                 });
             });
