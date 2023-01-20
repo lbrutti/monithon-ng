ad83384d75c5f055b85b7028df357ed4847dca74
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2023-01-20 16:51:12 +0100

    SM-104: rimosso mock da servizio recupero temi

--- src/app/services/monithonApiService/monithon-api.service.ts
+++ src/app/services/monithonApiService/monithon-api.service.ts
@@ -104,15 +104,15 @@ export class MonithonApiService {
     }
 
     //FIXME: lista temi da ws monithon: va passato query param con codice tema per filtraggio
-    public getTemi_REAL(ocCodTemaSintetico: string = ''): Observable<any> {
-        let url: string = this.url + '/mdTemi';
+    public getTemi_old(ocCodTemaSintetico: string = ''): Observable<any> {
+        let url: string = 'https://api.dev.monithon.eu/api/mdTemi';
         if (ocCodTemaSintetico.length) {
             url += `?tema=${ocCodTemaSintetico}`;
         }
         //{"4":[12,10,15,14,11,13,16],"6":[95,91,94,93,92],"5":[87,86,85,19,20,84,22,17,18,88,21,89,23,500],"7":[43]}
         // from(temiSintetici)
         // return this.httpClient.get<any>(this.url + '/mdTemi')
-        return this.httpClient.get<any>(this.url + '/mdTemi')
+        return this.httpClient.get<any>(url)
             .pipe(
                 map((res: any) => {
                     let temi: Tema[] = lodash.chain(res)
@@ -143,7 +143,16 @@ export class MonithonApiService {
             temiMock = {};
             lodash.set(temiMock, ocCodTemaSintetico, lodash.get(temiSintetici, ocCodTemaSintetico));
         }
-        return of(temiMock)
+        // return of(temiMock)
+        let url: string = 'https://api.dev.monithon.eu/api/mdTemi';
+        if (ocCodTemaSintetico.length) {
+            url += `?tema=${ocCodTemaSintetico}`;
+        }
+
+        //{"4":[12,10,15,14,11,13,16],"6":[95,91,94,93,92],"5":[87,86,85,19,20,84,22,17,18,88,21,89,23,500],"7":[43]}
+        // from(temiSintetici)
+        // return this.httpClient.get<any>(this.url + '/mdTemi')
+        return this.httpClient.get<any>(url)
             .pipe(
                 map((res: any) => {
                     let temi: Tema[] = lodash.chain(res)
